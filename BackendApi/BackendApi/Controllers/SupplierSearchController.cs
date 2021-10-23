a0e21281191b6532b0170cd4c7bbede2cfcd204f
spielyvo <spielyvo@students.zhaw.ch>
2021-10-23 10:26:36 +0200

    Updated getKeywordByFreeText

--- BackendApi/BackendApi/Controllers/SupplierSearchController.cs
+++ BackendApi/BackendApi/Controllers/SupplierSearchController.cs
@@ -13,6 +13,16 @@ namespace BackendApi.Controllers
     public class SupplierSearchController : ControllerBase
     {
         List<Solution> supplier = MockData.GetSuppliers();
+        Dictionary<string, string> keywords = MockData.GetKeywords();
+
+        private int GetInsertIndex(Models.KeywordMatch match, List<Models.KeywordMatch> sortedMatches)
+        {
+            for (int i = 0; i < sortedMatches.Count; i++)
+            {
+                if (match.Count > sortedMatches[i].Count) return i;
+            }
+            return sortedMatches.Count;
+        }
 
         [HttpGet("byKeyWord")]
         public List<Solution> GetByKeyWord([FromBody] string[] keyWords)
@@ -20,10 +30,34 @@ namespace BackendApi.Controllers
             return supplier;
         }
 
-        [HttpGet("byFreeText")]
-        public List<Solution> GetByFreeText([FromBody] string freeText)
+        [HttpGet("keyWordFromFreeText")]
+        public KeywordMatch[] GetKeyWordFromFreeText([FromBody] string freeText)
         {
-            return supplier;
+            char[] splitters = new char[] { ' ', ',', '.' };
+
+            var sortedKeywordMatches = new List<Models.KeywordMatch>();
+            var foundKeywords = new Dictionary<string, int>();
+
+            var words = freeText.Split(splitters);
+
+            foreach (var word in words)
+            {
+                string description;
+                if (keywords.TryGetValue(word, out description))
+                {
+                    if (!foundKeywords.ContainsKey(word)) foundKeywords[word] = 1;
+                    else foundKeywords[word] = foundKeywords[word] + 1;
+                }
+            }
+
+            foreach (var keyword in foundKeywords.Keys)
+            {
+                var match = new Models.KeywordMatch() { Keyword = keyword, Count = foundKeywords[keyword], Description = keywords[keyword] };
+                var index = GetInsertIndex(match, sortedKeywordMatches);
+                sortedKeywordMatches.Insert(index, match);
+            }
+
+            return sortedKeywordMatches.ToArray();
         }
 
 
--- BackendApi/BackendApi/MockData.cs
+++ BackendApi/BackendApi/MockData.cs
@@ -22,5 +22,46 @@ namespace BackendApi
             });
             return suppliers;
         }
+
+        public static Dictionary<string, string> GetKeywords()
+        {
+            Dictionary<string, string> Keywords = new Dictionary<string, string>();
+
+            //Bexio
+            Keywords.Add("accounting", "Tools to improve any financial task.");
+            Keywords.Add("trustee", "A person to administer your/customer property.");
+            Keywords.Add("customer", "Tools to help you manage your customers.");
+            Keywords.Add("order", "Tools to help you manage your orders.");
+
+            //Slack
+            Keywords.Add("communication", "Tools to imrove internal communication.");
+            Keywords.Add("teamwork", "Tools to improve collaboration of employees.");
+
+            //ClickTime
+            Keywords.Add("time", "Tools to record working hours and task scheduling.");
+            Keywords.Add("organisation", "Tools to help with organisation and time management.");
+
+            //HighRadius
+            Keywords.Add("billing", "Tools that manage invoices and exhortations.");
+            Keywords.Add("invoice", "Tools that manage invoices and exhortations.");
+            Keywords.Add("exhortation", "Tools that manage invoices and exhortations.");
+
+            //Parallel
+            Keywords.Add("logistics", "Tools to help manage your stock.");
+            Keywords.Add("stock", "Tools to help manage your stock.");
+            Keywords.Add("supply", "Tools to help you manage you supply chains.");
+
+            //Soxess
+            Keywords.Add("database", "Partners to build custom database solutions.");
+
+            //WebSamurai
+            Keywords.Add("website", "Tools and partners to build websites and improve web presence.");
+            Keywords.Add("marketing", "Tools and partners to help you with web presence and marketing");
+
+            //Misc
+            Keywords.Add("planning", "Try enter LOGISTICS if you are looking to organize your stock or TIME if you are looking to improve organisation");
+
+            return Keywords;
+        }
     }
 }
