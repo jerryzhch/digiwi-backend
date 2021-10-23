e28ba3cc05ce0716aa00b4bcbc04e7c04ee144ff
spielyvo <spielyvo@students.zhaw.ch>
2021-10-23 15:34:23 +0200

    Update SupplierSearchController.cs
    Changed string[] keyWords to a singel string;

--- BackendApi/BackendApi/Controllers/SupplierSearchController.cs
+++ BackendApi/BackendApi/Controllers/SupplierSearchController.cs
@@ -25,10 +25,14 @@ namespace BackendApi.Controllers
         }
 
         [HttpGet("byKeyWord")]
-        public List<Solution> GetByKeyWord(string[] keyWords)
+        public List<Solution> GetByKeyWord(string keyWords)
         {
             var foundSolutions = new Dictionary<Solution, int>();
-            foreach (var keyword in keyWords)
+
+            char[] splitters = new char[] { ';' };
+            var keys = keyWords.Split(splitters);
+
+            foreach (var keyword in keys)
             {
                 foreach (var solution in supplier)
                 {
