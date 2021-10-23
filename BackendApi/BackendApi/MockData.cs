a512c260a335e55b86cb7b6b32cae417d7a175d2
spielyvo <spielyvo@students.zhaw.ch>
2021-10-23 16:21:06 +0200

    Update MockData.cs
    change everything to lowercase

--- BackendApi/BackendApi/MockData.cs
+++ BackendApi/BackendApi/MockData.cs
@@ -18,8 +18,8 @@ namespace BackendApi
                 Address = "Alte Jonastrasse 24, 8640 Rapperswil-Jona",
                 Rating = 4,
                 Url = "https://www.bexio.com",
-                Keywords = new List<List<string>> { new List<string> { "Accounting", "5" },
-                    new List<string>{"Trustee", "4" }, new List<string>{"Customer", "2" }, new List<string>{"Order", "2" }},
+                Keywords = new List<List<string>> { new List<string> { "accounting", "5" },
+                    new List<string>{"trustee", "4" }, new List<string>{"customer", "2" }, new List<string>{"order", "2" }},
                 ImageUrl = "https://media-exp1.licdn.com/dms/image/C4D0BAQEEDvFk6_Ojrg/company-logo_200_200/0/1519904414454?e=1643241600&v=beta&t=SHERxu3cFzGTIfNbofkwXIUkLzSekd6tdEZ6Ly5sfps",
                 IsTool = false
 
@@ -28,7 +28,7 @@ namespace BackendApi
                 Name = "Slack",
                 Rating = 4,
                 Url = "https://slack.com",
-                Keywords = new List<List<string>> { new List<string> { "Communication", "4" }, new List<string> { "Teamwork", "4" }},
+                Keywords = new List<List<string>> { new List<string> { "communication", "4" }, new List<string> { "teamwork", "4" }},
                 ImageUrl = "https://upload.wikimedia.org/wikipedia/fr/thumb/7/7e/Slack_logo.svg/langfr-1024px-Slack_logo.svg.png",
                 IsTool = true
             },new Solution
@@ -36,8 +36,8 @@ namespace BackendApi
                 Name = "ClickTime",
                 Rating = 3,
                 Url = "https://www.clicktime.com/",
-                Keywords = new List<List<string>> { new List<string> { "Time", "4" }, new List<string> { "Organisation", "4" },
-                    new List<string>{ "Control", "3" } },
+                Keywords = new List<List<string>> { new List<string> { "time", "4" }, new List<string> { "organisation", "4" },
+                    new List<string>{ "control", "3" } },
                 ImageUrl = "https://marketplace.bamboohr.com/wp-content/uploads/2020/08/clicktime-logo-vertical-500px.png",
                 IsTool = true
             },new Solution
@@ -45,8 +45,8 @@ namespace BackendApi
                 Name = "HighRadius",
                 Rating = 4,
                 Url = "https://www.highradius.com/",
-                Keywords = new List<List<string>> { new List<string> { "Billing", "4" }, new List<string> { "Exhortation", "4" },
-                    new List<string>{"Invoice", "3"} },
+                Keywords = new List<List<string>> { new List<string> { "billing", "4" }, new List<string> { "exhortation", "4" },
+                    new List<string>{"invoice", "3"} },
                 ImageUrl = "https://mms.businesswire.com/media/20210503005090/en/875206/23/Highradius-logo_1_%282%29.jpg",
                 IsTool = true
             },new Solution
@@ -55,7 +55,7 @@ namespace BackendApi
                 Address = "Inseliquai 8, 6005 Luzern",
                 Rating = 4,
                 Url = "https://www.parallel.ch/",
-                Keywords = new List<List<string>> { new List<string> { "Logistics", "5" }, new List<string> { "Planning", "4" },
+                Keywords = new List<List<string>> { new List<string> { "logistics", "5" }, new List<string> { "planning", "4" },
                     new List<string>{"Stock", "4" }},
                 ImageUrl = "https://www.parallel.ch/_local/images/logo.png",
                 IsTool = false
@@ -66,7 +66,7 @@ namespace BackendApi
                 Address = "Sennweidstrasse 1b, 8608 Bubikon",
                 Rating = 5,
                 Url = "https://soxes.ch/",
-                Keywords = new List<List<string>> {new List<string> { "Database", "5"} },
+                Keywords = new List<List<string>> {new List<string> { "database", "5"} },
                 ImageUrl = "https://soxes.ch/wp-content/uploads/2021/08/soxes_dunkelgrau_190px_rgb.svg",
                 IsTool = false
 
@@ -76,7 +76,7 @@ namespace BackendApi
                 Address = "Binzstrasse 9, 8045 Zürich",
                 Rating = 5,
                 Url = "https://www.websamurai.ch",
-                Keywords = new List<List<string>> { new List<string> { "Website", "5"}, new List<string>{"Marketing", "4" } },
+                Keywords = new List<List<string>> { new List<string> { "website", "5"}, new List<string>{"marketing", "4" } },
                 ImageUrl = "https://www.websamurai.ch/wp-content/uploads/2018/10/WEBSAMURAI-mit-Claim-neg-500x134.png",
                 IsTool = false
 
