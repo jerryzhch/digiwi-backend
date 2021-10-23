46ac243da112b3e70f8dba11f3df2970f9915de2
spielyvo <spielyvo@students.zhaw.ch>
2021-10-23 11:38:48 +0200

    MockData added
    MockData Solution added;
    Url added to Solution class;

--- BackendApi/BackendApi/MockData.cs
+++ BackendApi/BackendApi/MockData.cs
@@ -11,13 +11,72 @@ namespace BackendApi
         public static List<Solution> GetSuppliers()
         {
             List<Solution> suppliers = new List<Solution>();
-            suppliers.Add(new Solution
+            suppliers.AddRange(new List<Solution> {
+                new Solution
             {
-                Name = "TEst",
-                Address = "Holtzweg 69",
+                Name = "Bexio",
+                Address = "Alte Jonastrasse 24, 8640 Rapperswil-Jona",
                 Rating = 5,
-                Keywords = new List<string> { "communication" },
-                ImageUrl = "www.thisisjustastest.gg"
+                Url = "https://www.bexio.com",
+                Keywords = new List<string> { "Accounting", "Trustee", "Customer", "Order" },
+                ImageUrl = "https://media-exp1.licdn.com/dms/image/C4D0BAQEEDvFk6_Ojrg/company-logo_200_200/0/1519904414454?e=1643241600&v=beta&t=SHERxu3cFzGTIfNbofkwXIUkLzSekd6tdEZ6Ly5sfps",
+                IsTool = false
+
+            }, new Solution
+            {
+                Name = "Slack",
+                Rating = 5,
+                Url = "https://slack.com",
+                Keywords = new List<string> { "Communication", "Teamwork"},
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/fr/thumb/7/7e/Slack_logo.svg/langfr-1024px-Slack_logo.svg.png",
+                IsTool = true
+            },new Solution
+            {
+                Name = "ClickTime",
+                Rating = 5,
+                Url = "https://www.clicktime.com/",
+                Keywords = new List<string> { "Time", "Organisation", "Control"},
+                ImageUrl = "https://marketplace.bamboohr.com/wp-content/uploads/2020/08/clicktime-logo-vertical-500px.png",
+                IsTool = true
+            },new Solution
+            {
+                Name = "HighRadius",
+                Rating = 5,
+                Url = "https://www.highradius.com/",
+                Keywords = new List<string> { "Billing", "Exhortation", "Invoice"},
+                ImageUrl = "https://mms.businesswire.com/media/20210503005090/en/875206/23/Highradius-logo_1_%282%29.jpg",
+                IsTool = true
+            },new Solution
+            {
+                Name = "Parallel",
+                Address = "Inseliquai 8, 6005 Luzern",
+                Rating = 5,
+                Url = "https://www.parallel.ch/",
+                Keywords = new List<string> { "Logistics", "Planning", "Stock" },
+                ImageUrl = "https://www.parallel.ch/_local/images/logo.png",
+                IsTool = false
+
+            },new Solution
+            {
+                Name = "Parallel",
+                Address = "Sennweidstrasse 1b, 8608 Bubikon",
+                Rating = 5,
+                Url = "https://soxes.ch/",
+                Keywords = new List<string> { "Database"},
+                ImageUrl = "https://soxes.ch/wp-content/uploads/2021/08/soxes_dunkelgrau_190px_rgb.svg",
+                IsTool = false
+
+            },new Solution
+            {
+                Name = "Parallel",
+                Address = "Binzstrasse 9, 8045 Zürich",
+                Rating = 5,
+                Url = "https://www.websamurai.ch",
+                Keywords = new List<string> { "Website", "Marketing"},
+                ImageUrl = "https://www.websamurai.ch/wp-content/uploads/2018/10/WEBSAMURAI-mit-Claim-neg-500x134.png",
+                IsTool = false
+
+            }
 
             });
             return suppliers;
